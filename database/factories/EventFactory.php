3193816318ea180f312c8a34337f7d1c099b5f15
Ani Emmanuel Kolise <anikolise@gmail.com>
2026-04-03 20:56:02 +0100

    fix:eventFactory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -17,17 +17,17 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
-        $title = $this->faker->sentence();
-        $location = $this->faker->city();
-        $isFree = $this->faker->boolean();
+        $title = fake()->unique()->sentence();
+        $location = fake()->city();
+        $isFree = fake()->boolean();
         return [
             'title' => $title,
             'description' => fake()->paragraph(),
             'location' => fake()->randomElement([$location, 'Remote']),
             'capacity' => fake()->numberBetween(70, 100),
             // Start the date in the future so it passes your "after:today" validation
-            'date' => $this->faker->dateTimeBetween('+1 week', '+1 year'),
-            'banner' => $this->faker->randomElement([
+            'date' => fake()->dateTimeBetween('+1 week', '+1 year'),
+            'banner' => fake()->randomElement([
                 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800&h=400&fit=crop', // Tech Conference
                 'https://images.unsplash.com/photo-1505373877841-8d25f7d46678?w=800&h=400&fit=crop', // Crowded Event
                 'https://images.unsplash.com/photo-1556761175-5973dc0f32e7?w=800&h=400&fit=crop', // Coding Meetup
@@ -36,14 +36,14 @@ class EventFactory extends Factory
             ]),
             'is_free' => $isFree,
             // If it's free, price is null. If paid, generate a random price between 5k and 50k
-            'price' => $isFree ? null : $this->faker->randomFloat(2, 5000, 50000),
+            'price' => $isFree ? null : fake()->randomFloat(2, 5000, 50000),
             'organizer_id' => fake()->randomElement(['9da3578e-d50a-45ed-af5a-aa27b4d9ee29', 'ba0818db-343c-4b2f-b20b-3ff3055b3b8e'])
         ];
     }
     public function onsite(): static
     {
         return $this->state(fn(array $attributes) => [
-            'location' => $this->faker->city(),
+            'location' => fake()->city(),
         ]);
     }
     public function remote(): static
@@ -64,7 +64,7 @@ class EventFactory extends Factory
     {
         return $this->state(fn(array $attributes) => [
             'is_free' => false,
-            'price' => $this->faker->randomFloat(2, 5000, 50000),
+            'price' => fake()->randomFloat(2, 5000, 50000),
         ]);
     }
 }
